9c42ce048f4297779bb68a359b53351c1f6eee19
lqdncel <chengyong_luo@163.com>
2016-01-11 15:53:26 +0800

    Initial Commit

---
+++ AVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVPlayer
+//
+//  Created by CY on 16/1/11.
+//  Copyright © 2016年 CheeryCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayer
+//
+//  Created by CY on 16/1/11.
+//  Copyright © 2016年 CheeryCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
